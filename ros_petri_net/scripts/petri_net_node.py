9c8a7e44396ed156408f951881d085d610ed726e
Christian Dondrup <c.dondrup@hw.ac.uk>
2018-06-08 16:05:35 +0100

    Removing old never used messages. Turning petri_net_node into action server.

--- ros_petri_net/scripts/petri_net_node.py
+++ ros_petri_net/scripts/petri_net_node.py
@@ -2,47 +2,114 @@
 # -*- coding: utf-8 -*-
 
 import rospy
+from actionlib import ActionServer
+from petri_net_msgs.msg import RPNAction, RPNResult
 from pnp_execution.executor import Executor
 import yaml
+import json
+from copy import deepcopy
+from rpn_ros_interface.ros_external_knowledge_base import ROSExternalKnowledgeBase as ROSKB
+from pnp_gen.generator import Generator
+from pnp_actions.pn_action import PNAction
+from pnp_actions.recovery import Recovery, Before, During, After
+import pnp_kb.queries as queries
+from pnp_gen.operations import BooleanAssertion
+import pnp_gen.operations as operations
+import pnp_gen.logical_operations as logical_operations
+from pprint import pprint
+import numpy as np
+import importlib
+import inspect
 
 
 class PetriNetNode(object):
     def __init__(self, name):
         rospy.loginfo("Starting '{}' ...".format(name))
-        with open(rospy.get_param("~plan"), 'r') as f:
-            self.plan = [x for x in yaml.load_all(f)]
-        print self.plan
-        exe = Executor()
-        exe.execute_net(exe.add_net(*self.create_net_from_plan(self.plan)))
+        self._as = ActionServer("/RPN", RPNAction, goal_cb=self.goal_cb, auto_start=False)
+        # with open(rospy.get_param("~plan"), 'r') as f:
+            # self.plan = [x for x in yaml.load_all(f)]
+        # print self.plan
+        # exe = Executor()
+        # exe.execute_net(exe.add_net(*self.create_net_from_plan(self.plan)))
         # exe.execute_net(exe.add_net(*self.test("test_net_1", {"value": 3})))
         # exe.execute_net(exe.add_net(*Generator().test("test_net_2", {"value": 4})))
+        self._as.start()
+        rospy.loginfo("Started '{}'.".format(name))
+
+    def goal_cb(self, gh):
+        gh.set_accepted()
+        goal = gh.get_goal()
+        exe = Executor()
+        exe.execute_net(exe.add_net(*self.create_net_from_plan(json.loads(goal.json_plan))))
+        gh.set_succeeded()
+
+    def __create_op(self, mutable, classes):
+        if isinstance(mutable, list):
+            for i, m in enumerate(mutable):
+                mutable[i] = self.__create_op(m, classes)
+        elif isinstance(mutable, dict):
+            for k in mutable.keys():
+                mutable[k] = self.__create_op(mutable[k], classes)
+            for k, v in classes.iteritems():
+                if k in mutable:
+                    return v(*(mutable.values()[0] if isinstance(mutable.values()[0], list) else [mutable.values()[0]]))
+        return mutable
+
+    def __create_action(self, action, action_definitions):
+        print "ACTION", action
+        name, params = action.items()[0]  # Only ever has one item
+        ad = action_definitions[name]
+        print "DEFINITION", ad
+
+        b = []; a = []
+
+        try:
+            b.append(Before(BooleanAssertion(ad["preconditions"], False), Recovery.SKIP_ACTION))
+        except KeyError:
+            pass
+
+        try:
+            b.append(Before(BooleanAssertion(ad["effects"], True), Recovery.SKIP_ACTION))
+            a.append(After(BooleanAssertion(ad["effects"], False), Recovery.RESTART_ACTION))
+        except KeyError:
+            pass
+
+        return PNAction(
+            ad["type"](name, params),
+            recovery=Recovery(
+                before=b,
+                after=a
+            )
+        )
+
+    def __create_concurrent_actions(self, actions, action_definitions):
+        res  = []
+        for action in actions.values()[0]:
+            if "concurrent_actions" in action:
+                res.append(self.__create_concurrent_actions(action, action_definitions))
+            else:
+                res.append(self.__create_action(action, action_definitions))
+        return res
+
+    def __parse_plan(self, cp, net, gen, plan, action_definitions):
+        for action in plan:
+            if "while" in action:
+                loop = action["while"]
+                acts = []
+                for a in loop["actions"]:
+                    acts.append(self.__create_action(a, action_definitions))
+                cond = BooleanAssertion(loop["condition"], True)
+                cp, net = gen.add_loop(net, cp, acts, cond)
+            elif "concurrent_actions" in action:
+                a = self.__create_concurrent_actions(action, action_definitions)
+                cp, net = gen.add_concurrent_actions(net, cp, a)
+            else:
+                a = self.__create_action(action, action_definitions)
+                cp, net = gen.add_action(net, cp, a)
+
+        return cp, net
 
     def create_net_from_plan(self, plan):
-        from rpn_ros_interface.ros_external_knowledge_base import ROSExternalKnowledgeBase as ROSKB
-        from pnp_gen.generator import Generator
-        from pnp_actions.pn_action import PNAction
-        from pnp_actions.recovery import Recovery, Before, During, After
-        import pnp_kb.queries as queries
-        from pnp_gen.operations import BooleanAssertion
-        import pnp_gen.operations as operations
-        import pnp_gen.logical_operations as logical_operations
-        from pprint import pprint
-        import numpy as np
-        import importlib
-        import inspect
-
-        def __create_op(mutable, classes):
-            if isinstance(mutable, list):
-                for i, m in enumerate(mutable):
-                    mutable[i] = __create_op(m, classes)
-            elif isinstance(mutable, dict):
-                for k in mutable.keys():
-                    mutable[k] = __create_op(mutable[k], classes)
-                for k, v in classes.iteritems():
-                    if k in mutable:
-                        return v(*(mutable.values()[0] if isinstance(mutable.values()[0], list) else [mutable.values()[0]]))
-            return mutable
-
         domain, plan = plan
 
         query_members = dict(inspect.getmembers(queries, inspect.isclass))
@@ -51,10 +118,18 @@ class PetriNetNode(object):
         for k, v in logical_operations_members.items():
             key = k.lower().replace('logical', '')
             logical_operations_members[key] = v
-        print logical_operations_members
 
         action_definitions = domain["actions"]
-        action_definitions = __create_op(__create_op(__create_op(action_definitions, query_members), operations_members), logical_operations_members)
+        action_definitions = self.__create_op(
+            self.__create_op(
+                self.__create_op(
+                    action_definitions,
+                    query_members
+                ),
+                operations_members
+            ),
+            logical_operations_members
+        )
         for k, v in action_definitions.iteritems():
             action_definitions[k]["type"] = getattr(importlib.import_module(v["type"]["module"]), v["type"]["class"])
         print " --- Action Definitons: --- "
@@ -63,67 +138,20 @@ class PetriNetNode(object):
         gen = Generator()
         cp, net = gen.create_net("test_net1", ROSKB, initial_knowledge=plan["initial_knowledge"])
 
-        plan = __create_op(__create_op(__create_op(plan["plan"], query_members), operations_members), logical_operations_members)
+        plan = self.__create_op(
+            self.__create_op(
+                self.__create_op(
+                    plan["plan"],
+                    query_members
+                ),
+                operations_members
+            ),
+            logical_operations_members
+        )
         print " --- Plan: --- "
         pprint(plan, width=120)
 
-        def __create_action(action, action_definitions):
-            print "ACTION", action
-            name, params = action.items()[0]  # Only ever has one item
-            ad = action_definitions[name]
-            print "DEFINITION", ad
-
-            b = []; a = []
-
-            try:
-                b.append(Before(BooleanAssertion(ad["preconditions"], False), Recovery.SKIP_ACTION))
-            except KeyError:
-                pass
-
-            try:
-                b.append(Before(BooleanAssertion(ad["effects"], True), Recovery.SKIP_ACTION))
-                a.append(After(BooleanAssertion(ad["effects"], False), Recovery.RESTART_ACTION))
-            except KeyError:
-                pass
-
-            return PNAction(
-                ad["type"](name, params),
-                recovery=Recovery(
-                    before=b,
-                    after=a
-                )
-            )
-
-        def __create_concurrent_actions(actions, action_definitions):
-            res  = []
-            for action in actions.values()[0]:
-                if "concurrent_actions" in action:
-                    res.append(__create_concurrent_actions(action, action_definitions))
-                else:
-                    res.append(__create_action(action, action_definitions))
-            return res
-
-        def __parse_plan(cp, net, plan, action_definitions):
-            for action in plan:
-                if "while" in action:
-                    print action
-                    loop = action["while"]
-                    print loop
-                    acts = []
-                    for a in loop["actions"]:
-                        acts.append(__create_action(a, action_definitions))
-                    cond = BooleanAssertion(loop["condition"], True)
-                    cp, net = gen.add_loop(net, cp, acts, cond)
-                elif "concurrent_actions" in action:
-                    a = __create_concurrent_actions(action, action_definitions)
-                    cp, net = gen.add_concurrent_actions(net, cp, a)
-                else:
-                    a = __create_action(action, action_definitions)
-                    cp, net = gen.add_action(net, cp, a)
-
-            return cp, net
-
-        cp, net = __parse_plan(cp, net, plan, action_definitions)
+        cp, net = self.__parse_plan(cp, net, gen, plan, action_definitions)
         cp, net = gen.add_goal(net, cp)
         pprint(net)
         marking = np.zeros(net.num_places, dtype=int)
@@ -137,7 +165,8 @@ class PetriNetNode(object):
         from pnp_gen.generator import Generator
         from pnp_actions.pn_action import PNAction
         from pnp_actions.recovery import Recovery, Before, During, After
-        from pnp_actions.queries import BooleanAssertion, Comparison, LocalQuery, RemoteQuery, Query
+        from pnp_kb.queries import LocalQuery, RemoteQuery, Query
+        from pnp_gen.operations import BooleanAssertion, Comparison
         from pprint import pprint
         import numpy as np
 
@@ -221,5 +250,5 @@ class PetriNetNode(object):
 if __name__ == "__main__":
     rospy.init_node("ros_petri_net_node")
     p = PetriNetNode(rospy.get_name())
-    # rospy.spin()
+    rospy.spin()
 
---
+++ ros_petri_net/scripts/rpn_test_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from actionlib import SimpleActionClient
+from petri_net_msgs.msg import RPNAction, RPNGoal
+import yaml
+import json
+
+
+class TestClient(object):
+    def __init__(self, name):
+        rospy.loginfo("Starting '{}'...".format(name))
+        self.client = SimpleActionClient("/RPN", RPNAction)
+        self.client.wait_for_server()
+        with open(rospy.get_param("~plan"), 'r') as f:
+            self.plan = [x for x in yaml.load_all(f)]
+        self.client.send_goal_and_wait(RPNGoal(json_plan=json.dumps(self.plan)))
+        rospy.loginfo("Started '{}'.".format(name))
+
+
+if __name__ == "__main__":
+    rospy.init_node("rpn_test_client")
+    t = TestClient(rospy.get_name())
+    rospy.spin()
+
