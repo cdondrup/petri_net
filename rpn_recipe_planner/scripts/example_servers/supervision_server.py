eb87b80733cf4b4a3535cce2ad773e48edeae0ff
Christian Dondrup <cdondrup@gmail.com>
2019-06-28 09:37:37 +0100

    Using action in the supervision server.

--- rpn_recipe_planner/scripts/example_servers/supervision_server.py
+++ rpn_recipe_planner/scripts/example_servers/supervision_server.py
@@ -9,7 +9,8 @@ from rpn_recipe_planner_msgs.srv import SuperQuery, SuperQueryResponse, SuperInf
 from dialogue_arbiter.msg import DialogueArbiterActionResult
 from threading import Thread
 from collections import OrderedDict
-from guiding_as.msg import taskActionResult
+from guiding_as_msgs.msg import taskGoal, taskAction, taskActionResult
+from actionlib import SimpleActionClient
 import json
 
 
@@ -22,22 +23,22 @@ class TestServer(object):
             auto_start=False
         )
         self.threads = OrderedDict({})
-        self.pub = rospy.Publisher("/supervisor/new_goal", Task, queue_size=10)
-        self.sub = rospy.Subscriber("/guiding_task/result", taskActionResult, self.result_cb)
+        # self.pub = rospy.Publisher("/supervisor/new_goal", Task, queue_size=10)
+        # self.sub = rospy.Subscriber("/guiding_task/result", taskActionResult, self.result_cb)
         self.srv = rospy.Service("~inform", SuperInform, lambda x: self.srv_cb(x, "inform"))
         self.srv = rospy.Service("~query", SuperQuery, lambda x: self.srv_cb(x, "query"))
         self._ps.start()
 
-    def result_cb(self, msg):
-        print "RESULT MSG", msg
-        gh = self.threads.items()[-1][0]
-        if msg.result.success:
-            gh.set_succeeded()
-        else:
-            gh.set_aborted()
-        print self.threads
-        del self.threads[gh]
-        print self.threads
+    # def result_cb(self, msg):
+        # print "RESULT MSG", msg
+        # gh = self.threads.items()[-1][0]
+        # if msg.result.success:
+            # gh.set_succeeded()
+        # else:
+            # gh.set_aborted()
+        # print self.threads
+        # del self.threads[gh]
+        # print self.threads
 
     def goal_cb(self, gh):
         print "received goal"
@@ -49,9 +50,20 @@ class TestServer(object):
         self.threads[gh].start()
 
     def execute(self, gh, goal):
-        t = Task(place_frame=goal.place_frame, person_frame='human-0')
-        print "sending message"
-        self.pub.publish(t)
+        sg = taskGoal(place_frame=goal.place_frame, person_frame=goal.person_frame)
+        client = SimpleActionClient("/guiding_task", taskAction)
+        client.wait_for_server()
+        client.send_goal_and_wait(sg)
+        result = client.get_result()
+        print "RESULT", result
+        if result is not None:
+            if result.success:
+                gh.set_succeeded()
+                return
+        gh.set_aborted()
+        # t = Task(place_frame=goal.place_frame, person_frame='human-0')
+        # print "sending message"
+        # self.pub.publish(t)
 
     def srv_cb(self, req, type):
         gh = self.threads.items()[-1][0]
